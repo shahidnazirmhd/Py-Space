8988f9275b2ec1c808495eb18b5fb4731539099c
shahidnazirmhd <shahidnazirmhd@gmail.com>
2023-04-28 19:48:28 +0530

    Apr 28

--- Py-Basics/intro.py
+++ Py-Basics/intro.py
@@ -3,29 +3,29 @@
 # message = 'Hello world'
 # print(message[0:5])
 
-# import datetime
+import datetime
 
-# date = datetime.datetime(2000, 2, 28, 23, 30, 15)
-# print(type(date))
-# print(date)
+date = datetime.datetime(2000, 2, 28, 23, 30, 15)
+print(type(date))
+print(date)
 
-# sentence = 'My Birth date is {0:%B %d, %Y} fell on a {0:%A} and was the {0:%j} day of the year'.format(date)
-# print(sentence)
-# print(type(format(date)))
+sentence = 'My Birth date is {0:%B %d, %Y} fell on a {0:%A} and was the {0:%j} day of the year'.format(date)
+print(sentence)
+print(type(format(date)))
 
-year ='2000'
-date = '28'
-month = 'February'
-day = 'Monday'
-days_of_year = '059'
-
-sentence2 = f'My Birth date is {month+" "+date+", "+year} fell on a {day} and was the {days_of_year} day of the year'
-print(sentence2)
-
-name = 'Shahid'
-
-print(dir(name))
-
-print(help(str))
-
-print(help(str.lower))
+# year ='2000'
+# date = '28'
+# month = 'February'
+# day = 'Monday'
+# days_of_year = '059'
+#
+# sentence2 = f'My Birth date is {month+" "+date+", "+year} fell on a {day} and was the {days_of_year} day of the year'
+# print(sentence2)
+#
+# name = 'Shahid'
+#
+# print(dir(name))
+#
+# print(help(str))
+#
+# print(help(str.lower))
